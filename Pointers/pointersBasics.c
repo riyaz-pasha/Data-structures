b1ecc43b7cb7c7c21b9afc25354059c795b8eebb
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-02 16:34:06 +0530

    [Riyaz] Add. mutating value of normal variable using pointer variable

--- Pointers/pointersBasics.c
+++ Pointers/pointersBasics.c
@@ -5,9 +5,18 @@ int main(int argc, char const *argv[])
 
     printf("address of Variable(&)-%p\n\n", &normalVariable);
 
+    printf("\n**************************************************************\n");
     normalVariable = 1;
     int *integerTypePointerVariable;
     integerTypePointerVariable = &normalVariable;
+    printf("normalVariable-%d\n", normalVariable);
+    printf("normalVariable address-%p\n", &normalVariable);
+    printf("integerTypePointerVariable-%p\n", integerTypePointerVariable);
+    printf("*integerTypePointerVariable-%d\n", *integerTypePointerVariable);
+    printf("&integerTypePointerVariable-%p\n", &integerTypePointerVariable);
+
+    printf("\n**************************************************************\n");
+    *integerTypePointerVariable = 2;
     printf("normalVariable-%d\n", normalVariable);
     printf("normalVariable address-%p\n", &normalVariable);
     printf("integerTypePointerVariable-%p\n", integerTypePointerVariable);
