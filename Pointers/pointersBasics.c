5431235516d91ec1d156179f1c28750de6112d5a
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-02 16:30:28 +0530

    [Riyaz] Add. int type pointer variable

--- Pointers/pointersBasics.c
+++ Pointers/pointersBasics.c
@@ -3,6 +3,15 @@ int main(int argc, char const *argv[])
 {
     int normalVariable;
 
-    printf("address of Variable(&)-%p", &normalVariable);
+    printf("address of Variable(&)-%p\n\n", &normalVariable);
+
+    normalVariable = 1;
+    int *integerTypePointerVariable;
+    integerTypePointerVariable = &normalVariable;
+    printf("normalVariable-%d\n", normalVariable);
+    printf("normalVariable address-%p\n", &normalVariable);
+    printf("integerTypePointerVariable-%p\n", integerTypePointerVariable);
+    printf("*integerTypePointerVariable-%d\n", *integerTypePointerVariable);
+    printf("&integerTypePointerVariable-%p\n", &integerTypePointerVariable);
     return 0;
 }
