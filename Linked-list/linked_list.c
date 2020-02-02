aea6aa36d3e6a419768d1ae502689e34b54dfd76
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-02 22:56:13 +0530

    [Riyaz] Add. appends Node at the end of the list

--- Linked-list/linked_list.c
+++ Linked-list/linked_list.c
@@ -19,6 +19,27 @@ void push(struct Node **headNode_ref, int new_data)
     *headNode_ref = new_node;
 }
 
+void append(struct Node **headNode_ref, int new_data)
+{
+    //headNode_ref--- head node address-- this will be constant
+    //*headNode_ref== headnode -current head node -- this will vary whenever head node changes
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *last_node = *headNode_ref;
+    new_node->data = new_data;
+    new_node->nextNode = NULL;
+
+    if (*headNode_ref == NULL)
+    {
+        *headNode_ref = new_node;
+        return;
+    }
+
+    while (last_node->nextNode != NULL)
+        last_node = last_node->nextNode;
+    last_node->nextNode = new_node;
+    return;
+}
+
 int main(int argc, char const *argv[])
 {
     struct Node *headNode = NULL;
@@ -33,6 +54,11 @@ int main(int argc, char const *argv[])
     printf("headNode->data-%d\n", headNode->data);
     printf("headNode-%p\n", headNode);
     printf("headNode->nextNode-%p\n", headNode->nextNode);
+
+    printf("\n**************************************************************\n");
+
+    append(&headNode, 33);
+    printf("headNode->nextNode->nextNode->data-%d\n", headNode->nextNode->nextNode->data);
 
     printf("\n**************************************************************\n");
 
