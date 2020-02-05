3a15337a79774bfa46d528793b827cdbe89b7fd3
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-05 23:36:19 +0530

    [Riyaz] Add. Prints list using recursion

---
+++ Linked-list/linked_list_length.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *nextNode;
+};
+
+void push(struct Node **head_node_ref, int new_data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->nextNode = *head_node_ref;
+    *head_node_ref = new_node;
+    return;
+}
+
+void printListRecusrion(struct Node *node)
+{
+    if (node == NULL)
+        return;
+    printf("%d\n", node->data);
+    return printListRecusrion(node->nextNode);
+}
+
+int main(int argc, char const *argv[])
+{
+    struct Node *head_node;
+
+    push(&head_node, 5);
+    push(&head_node, 4);
+    push(&head_node, 3);
+    push(&head_node, 2);
+    push(&head_node, 1);
+
+    printListRecusrion(head_node);
+    printf("\n**************************************************************\n");
+
+    return 0;
+}
