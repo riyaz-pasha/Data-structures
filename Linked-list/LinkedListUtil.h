4f4864fbf7f412279bbb1a02bcfc6528669fa176
riyazpasha <riyazpasha.mohammed@thoughtworks.com>
2020-02-07 22:57:14 +0530

    [Riyaz] Add. Linked list utils

---
+++ Linked-list/LinkedListUtil.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *nextNode;
+};
+void push(struct Node **head_node_ref, int new_data)
+{
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->nextNode = *head_node_ref;
+    *head_node_ref = new_node;
+}
+
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d\n", node->data);
+        node = node->nextNode;
+    }
+    printf("\n**************************************************************\n");
+}
