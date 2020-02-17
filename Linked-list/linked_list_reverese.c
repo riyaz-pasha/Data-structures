6bfd643af5c710d28f67b0af70c5db9584fcedff
riyazpasha <riyazpasha880@gmail.com>
2020-02-17 22:47:03 +0530

    [Riyaz] Add. Reverse Linked list

--- Linked-list/linked_list_reverese.c
+++ Linked-list/linked_list_reverese.c
@@ -8,6 +8,8 @@ struct Node
 
 void push(struct Node **, int);
 void printListUsingRecursion(struct Node *node);
+void reverseLinkedList(struct Node **);
+
 int main(int argc, char const *argv[])
 {
     struct Node *head_node;
@@ -17,7 +19,30 @@ int main(int argc, char const *argv[])
     push(&head_node, 2);
     push(&head_node, 1);
     printListUsingRecursion(head_node);
+
+    reverseLinkedList(&head_node);
+    printListUsingRecursion(head_node);
+
+    push(&head_node, 6);
+    printListUsingRecursion(head_node);
     return 0;
+}
+
+void reverseLinkedList(struct Node **head_node_ref)
+{
+    struct Node *prev_node = NULL,
+                *current_node = *head_node_ref,
+                *next_node = NULL;
+    while (current_node != NULL)
+    {
+        next_node = current_node->nextNode;
+
+        current_node->nextNode = prev_node;
+
+        prev_node = current_node;
+        current_node = next_node;
+    }
+    *head_node_ref = prev_node;
 }
 
 void push(struct Node **head_node_ref, int new_data)
